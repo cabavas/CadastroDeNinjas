3a773ef4ad0b0d4e08d196a4ffdbcdcb03d9b5fd
cabavas <cabavas@gmail.com>
2024-12-13 14:29:57 -0300

    Aula 24

--- src/main/java/com/example/CadastroDeNinjas/Ninjas/NinjaService.java
+++ src/main/java/com/example/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -23,4 +23,8 @@ public class NinjaService {
         Optional<NinjaModel> ninjaPorId = ninjaRepository.findById(id);
         return ninjaPorId.orElse(null);
     }
+
+    public NinjaModel crearNinja(NinjaModel ninja) {
+        return ninjaRepository.save(ninja);
+    }
 }
